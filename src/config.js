4d88392836050d82278042a0dd8017d377f103d4
Renuja-7671 <renujajanith7671@gmail.com>
2025-07-23 14:45:30 +0530

    config changed

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-//const STRIPE_PUBLISHABLE_KEY = "pk_test_51RirFIFwBYKnL5Z3swzBJJ1N9TUIe9fIJKvXcnw6NH1V3pSWagHy9fHBiuIbMAEsKj6rvxIyUyGBde8CCvy0arXB00ToVFYydf"
-//const REACT_APP_API_BASE_URL= "https://courtfindbackend-production.up.railway.app"
+const STRIPE_PUBLISHABLE_KEY = "pk_test_51RirFIFwBYKnL5Z3swzBJJ1N9TUIe9fIJKvXcnw6NH1V3pSWagHy9fHBiuIbMAEsKj6rvxIyUyGBde8CCvy0arXB00ToVFYydf"
+const REACT_APP_API_BASE_URL= "https://courtfindbackend-production.up.railway.app"
 //const REACT_APP_API_BASE_URL= "http://localhost:8000"
 
 
-const REACT_APP_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
+//const REACT_APP_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 
 export { REACT_APP_API_BASE_URL, STRIPE_PUBLISHABLE_KEY };
 
